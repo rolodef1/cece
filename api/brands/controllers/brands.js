a68c0650d0984a8517f9659f51da8da8db9f7e25
Isaac Carrera <rolodef1@gmail.com>
2020-08-07 15:02:57 -0500

    Tercer commit

--- api/brands/controllers/brands.js
+++ api/brands/controllers/brands.js
@@ -4,5 +4,32 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
 
-module.exports = {};
+module.exports = {
+    async update(ctx) {
+        const { id } = ctx.params;
+    
+        let entity;
+    
+        const [brand] = await strapi.services.brands.find({
+          id: ctx.params.id,
+          'user.id': ctx.state.user.id,
+        });
+    
+        if (!brand) {
+          return ctx.unauthorized(`You can't update this entry`);
+        }
+    
+        if (ctx.is('multipart')) {
+          const { data, files } = parseMultipartData(ctx);
+          entity = await strapi.services.brands.update({ id }, data, {
+            files,
+          });
+        } else {
+          entity = await strapi.services.brands.update({ id }, ctx.request.body);
+        }
+    
+        return sanitizeEntity(entity, { model: strapi.models.brands });
+    },
+};
--- api/promotion/controllers/promotion.js
+++ api/promotion/controllers/promotion.js
@@ -5,4 +5,98 @@
  * to customize this controller
  */
 
-module.exports = {};
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+   /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+
+    async create(ctx) {
+        let entity;
+        //Busca la marca asociada al usuario autenticado
+        const [brand] = await strapi.services.brands.find({
+            'user.id': ctx.state.user.id,
+        });
+        //Si no se encontro una marca entonces retorna error
+        if (!brand) {
+            return ctx.unauthorized(`You can't create this entry`);
+        }
+        //Si existen archivos en el request
+        if (ctx.is('multipart')) {
+            //Obtiene la data y los archios desde el request
+            const { data, files } = parseMultipartData(ctx);            
+            //Establece por defecto que la marca de la promocion sea la asociada al usuario autenticado
+            data.brand = brand.id;
+            //Crea la entidad con los datos y archivos
+            entity = await strapi.services.promotion.create(data, { files });
+        } else {
+            //Establece por defecto que la marca de la promocion sea la asociada al usuario autenticado
+            ctx.request.body.brand = brand.id;
+            //Crea la entidad con los datos
+            entity = await strapi.services.promotion.create(ctx.request.body);
+        }
+        //Registra la entidad
+        return sanitizeEntity(entity, { model: strapi.models.promotion });
+    },
+
+    /**
+     * Update a record.
+     *
+     * @return {Object}
+     */
+    async update(ctx) {
+        //Obtiene el id del registro que se esta intentando actualizar
+        const { id } = ctx.params;
+
+        let entity;
+        //Obtiene la promocion que tenga el id y su marca pertenezca al usuario autenticado
+        const [promotion] = await strapi.services.promotion.find({
+            id: ctx.params.id,
+            'brand.user.id': ctx.state.user.id,
+        });
+        //Si no se encontro la promocion entonces retorna error
+        if (!promotion) {
+            return ctx.unauthorized(`You can't update this entry`);
+        }
+        //Si existen archivos en el request
+        if (ctx.is('multipart')) {
+            //Obtiene la data y los archios desde el request
+            const { data, files } = parseMultipartData(ctx);
+            //Establece por defecto que la marca de la promocion sea siempre la misma
+            data.brand = promotion.brand.id;
+            //Crea la entidad con los datos y archivos
+            entity = await strapi.services.promotion.update({ id }, data, {
+                files,
+            });
+        } else {
+            //Establece por defecto que la marca de la promocion sea siempre la misma
+            ctx.request.body.brand = promotion.brand.id;
+            //Crea la entidad con los datos
+            entity = await strapi.services.promotion.update({ id }, ctx.request.body);
+        }
+        //Registra la entidad
+        return sanitizeEntity(entity, { model: strapi.models.promotion });
+    },
+
+    /**
+     * Destroy a record.
+     *
+     * @return {Object}
+     */
+    async delete(ctx) {
+        //Obtiene la promocion que tenga el id y su marca pertenezca al usuario autenticado
+        const [promotion] = await strapi.services.promotion.find({
+            id: ctx.params.id,
+            'brand.user.id': ctx.state.user.id,
+        });
+        //Si no se encontro la promocion entonces retorna error
+        if (!promotion) {
+            return ctx.unauthorized(`You can't update this entry`);
+        }
+        const entity = await strapi.services.promotion.delete({ id: ctx.params.id });
+        return sanitizeEntity(entity, { model: strapi.models.promotion });
+    },
+};
