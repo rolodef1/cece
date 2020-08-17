c6ceac636e61654e4bd05bd9c885e6c279cf20ae
Isaac Carrera <rolodef1@gmail.com>
2020-08-16 20:02:04 -0500

    Configuracion servidor produccion

--- server.js
+++ server.js
@@ -1,14 +1,3 @@
 const strapi = require('strapi');
-const throng = require('throng');
 
-const WORKERS = process.env.WEB_CONCURRENCY || 1
-const PORT = process.env.PORT || 3000
-
-throng({
-  workers: WORKERS,
-  lifetime: Infinity
-}, start)
-
-function start() {
-    strapi(/* {...} */).start();
-}
+strapi(/* {...} */).start();
